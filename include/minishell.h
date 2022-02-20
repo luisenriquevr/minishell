d5c708fc79e4762a31f5b23adb5f6bee646009a8
cristiana-marcu <cristiana.marcu.n@gmail.com>
2022-02-20 17:50:31 +0100

    Already filling command list

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: cmarcu <cmarcu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 19:23:05 by cmarcu            #+#    #+#             */
-/*   Updated: 2022/02/19 18:36:30 by cmarcu           ###   ########.fr       */
+/*   Updated: 2022/02/20 12:47:26 by cmarcu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define MINISHELL_H
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <readline/readline.h>
 #include <stdbool.h>
 
@@ -21,9 +22,9 @@
 * Hay que declarar todas las listas antes de definirlas para que el *next pueda
 * ser del tipo de la lista
 */
-typedef struct s_command_line t_cmd_line;
+typedef struct s_cmd_line t_cmd_line;
 
-struct s_command_line
+struct s_cmd_line
 {
 	char 		*str;
 	t_cmd_line	*next;
--- srcs/readlinepruebas.c
+++ srcs/readlinepruebas.c
@@ -6,11 +6,12 @@
 /*   By: cmarcu <cmarcu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 18:36:17 by cmarcu            #+#    #+#             */
-/*   Updated: 2022/02/19 19:16:45 by cmarcu           ###   ########.fr       */
+/*   Updated: 2022/02/20 14:24:07 by cmarcu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+#include <readline/readline.h>
 
 int exit_status; //Variable global
 
@@ -68,35 +69,127 @@ void check_str(char *str)
 	check_quotes(str);
 }
 
-int get_cmd_line(char *str, t_cmd_line **cmd_line)
+void look_for_pipe(char *str, int *current_position)
 {
-	t_cmd_line	*new;
+	t_quote quote;
 
-	if (str != NULL && *cmd_line != NULL)
+	quote = NONE;
+	while (str[*current_position])
 	{
-		new = malloc(sizeof(t_cmd_line));
-		if (new == NULL)
-			return (-1);
-		new->str = NULL;
-		new->next = NULL;
-		//Tengo que recorrer str en busca de | (comprobando que no esté dentro
-		//de comillas), y guardar el índice de inicio y final del trozo de string
-		//que esté antes del pipe y hacer un malloc de eso y un strncpy de eso
-		//en el str de la estructura de command_line.
-		//posicion_actual_string es el índice del string en el que hemos encontrado un pipe
-		//inicio_comando es el índice del string en el que empieza el comando
-		// hola | quease
-		// ^    ^
-		//inicio_comando es el primer gorrito y posicion_actual_comando es el segundo
-		new->str = malloc(sizeof(char) * (posicion_actual_string - inicio_comando + 1));
-		//Comprobar el fallo del malloc
-		new->str = ft_strncpy(new->string, str + inicio_comando, posicion_actual_string - inicio_comando);
-		//Con una de las funciones de listas añadir al final de cmd_line el puntero a new como next
-		ft_lstadd_back(*cmd_line, new);
+		if (str[*current_position] == '"')
+		{
+			if (quote == NONE)
+				quote = DOUBLE;
+			else if (quote == DOUBLE)
+				quote = NONE;
+		}
+		if (str[*current_position] == '\'')
+		{
+			if (quote == NONE)
+				quote = SINGLE;
+			else if (quote == SINGLE)
+				quote = NONE;
+		}
+		if (str[*current_position] == '|' && quote == NONE)
+			return ;
+		(*current_position)++;
 	}
 }
 
-int main(int argc, char **argv, char **env)
+char	*ft_strncpy(char *dst, char *src, int n)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] && i < n)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+t_cmd_line	*ft_lstlast(t_cmd_line *lst)
+{
+	while (lst)
+	{
+		if (!lst->next)
+			return (lst);
+		lst = lst->next;
+	}
+	return (lst);
+}
+
+void	ft_lstadd_back(t_cmd_line **lst, t_cmd_line *new)
+{
+	t_cmd_line *last;
+
+	if (lst)
+	{
+		if (*lst)
+		{
+			last = ft_lstlast(*lst);
+			last->next = new;
+		}
+		else
+			*lst = new;
+	}
+}
+
+void fill_cmd_list(t_cmd_line **cmd_line, char *str, int end, int start)
+{
+	t_cmd_line	*new;
+
+	new = malloc(sizeof(t_cmd_line));
+	if (new == NULL)
+		exit_status = 4;
+	new->str = NULL;
+	new->next = NULL;
+	new->str = malloc(sizeof(char) * (end - start + 1));
+	if (new->str == NULL)
+		exit_status = 4;
+	new->str = ft_strncpy(new->str, str + start, end - start);
+	ft_lstadd_back(cmd_line, new);
+}
+
+void get_cmd_line(char *str, t_cmd_line **cmd_line)
+{
+	int curr_pos;
+	int cmd_start;
+
+	curr_pos = 0;
+	cmd_start = 0;
+	if (str != NULL)
+	{
+		while (str[curr_pos])
+		{
+			look_for_pipe(str, &curr_pos);
+			fill_cmd_list(cmd_line, str, curr_pos, cmd_start);
+			cmd_start = curr_pos;
+			cmd_start++;
+			curr_pos++;
+		}
+	}
+}
+
+void	ft_lstiter(t_cmd_line *lst, void (*f)(char *))
+{
+	if (!lst || !f)
+		return ;
+	while (lst)
+	{
+		(*f)(lst->str);
+		lst = lst->next;
+	}
+}
+
+void print_list(char *str)
+{
+	printf("%s\n", str);
+}
+
+int main(void)
 {
 	char			*str;
 	t_cmd_line	*cmd_line;
@@ -109,17 +202,8 @@ int main(int argc, char **argv, char **env)
 		add_history(str); //Esto añade la historia de comandos, hace que funcione la flecha para arriba
 		check_str(str);
 		get_cmd_line(str, &cmd_line);
+		ft_lstiter(cmd_line, print_list);
 		free(str);
 	}
+	return (0);
 }
-
-/*
-* Desde el main, después de comprobar que el str de readline no es null y de chequear el str para
-* que el primer carácter distinto de espacio no sea un pipe y que las comillas estén cerradas
-*
-*
-*
-*
-*
-*
-*/

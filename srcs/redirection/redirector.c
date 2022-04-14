1da4d929c459fc2fff9fec78b4063df401e65ecd
lvarela <lvarela@student.42madrid.com>
2022-04-14 19:13:34 +0200

    new changes

--- srcs/redirection/redirector.c
+++ srcs/redirection/redirector.c
@@ -1,30 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtin_exit.c                                     :+:      :+:    :+:   */
+/*   redirector.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lvarela <lvarela@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/13 17:21:51 by lvarela           #+#    #+#             */
-/*   Updated: 2022/04/14 19:11:49 by lvarela          ###   ########.fr       */
+/*   Created: 2022/04/14 18:23:51 by lvarela           #+#    #+#             */
+/*   Updated: 2022/04/14 18:27:39 by lvarela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-int			builtin_exit(char **cmd)
-{
-	if (array_length(cmd) > 2)
-		return (throw_error_exit_status("Error: Too many arguments", 1, 1));
-	printf("exit\n");
-	if (!ft_isdigit(cmd[1][0]))
-	{
-		throw_error("Error: Bad expression");
-		exit(255);
-	}
-	if (cmd[1])
-		exit(EXIT_SUCCESS);
-	else
-		exit(ft_atoi(cmd[1]));
-	return (1);
-}
+void		
